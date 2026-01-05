1f05749274917efbbbdaf397a2069e499f3e0ec6
hassaan506 <69252452+hassaan506@users.noreply.github.com>
2026-01-05 21:20:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "edeetos-v13"; // Changed name to force update
+const CACHE_NAME = "edeetos-v14"; // Changed name to force update
 const ASSETS_TO_CACHE = [
   "./",                // The root folder
   "./index.html",      // The main file
