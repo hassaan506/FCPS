32c9972e1cbd2b6d53d2638aedac6189fc84bf72
hassaan506 <69252452+hassaan506@users.noreply.github.com>
2026-01-20 00:56:40 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "edeetos-v44"; // Bump version to force update
+const CACHE_NAME = "edeetos-v45"; // Bump version to force update
 
 // Only local, guaranteed assets
 const ASSETS_TO_CACHE = [
